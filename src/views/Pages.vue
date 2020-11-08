8e993e512ee1c67384c84cb4712fb0825eae15c4
Eugene van zyl <eugene.vanzyl@mintgroup.net>
2020-11-08 09:13:44 +0200

    Left pane details

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -1,46 +1,55 @@
 <template>
   <div>
     <v-container>
-      <v-card class="mx-auto" outlined>
-        <v-row style="align-items: center; padding-left: 24px">
-          <v-col><v-btn @click="$router.go(-1)" icon><v-icon>mdi-arrow-left</v-icon></v-btn></v-col>
-          <v-col>
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-subtitle>Filename</v-list-item-subtitle>
-                <v-list-item-title>{{ $route.params.data.name }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-          <v-col>
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-subtitle>User</v-list-item-subtitle>
-                <v-list-item-title>{{ $route.params.data.user }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-          <v-col>
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-subtitle>Status</v-list-item-subtitle>
-                <v-list-item-title>{{ $route.params.data.status }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-          <v-col>
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-subtitle>Upload date</v-list-item-subtitle>
-                <v-list-item-title>{{ $route.params.data.upload }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-        </v-row>
-      </v-card>
       <v-row>
-        <v-col cols="12" sm="6" md="4" v-for="link in links" :key="link.filepath">
-          <Page :data="link"/>
+        <v-col cols="12" sm="2">
+          <v-list>
+            <v-list-item-group>
+              <v-list-item @click="$router.go(-1)">
+                <v-list-item-avatar>
+                  <v-icon>mdi-arrow-left-thin-circle-outline</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title> Back</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-subtitle>Filename</v-list-item-subtitle>
+                  <v-list-item-title>{{ $route.params.data.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-subtitle>User</v-list-item-subtitle>
+                  <v-list-item-title>{{ $route.params.data.user }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-subtitle>Status</v-list-item-subtitle>
+                  <v-list-item-title>{{ $route.params.data.status }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item two-line>
+                <v-list-item-content>
+                  <v-list-item-subtitle>Upload date</v-list-item-subtitle>
+                  <v-list-item-title>{{ $route.params.data.upload }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
+        <v-col cols="12" sm="10">
+          <v-row>
+            <v-col cols="12" sm="6" md="4" v-for="link in links" :key="link.filepath">
+              <Page :data="link"/>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
